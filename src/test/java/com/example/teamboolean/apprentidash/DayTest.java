1e9083900b4fda113e1bfa7d88ee07b5a2a0746e
Jorie Fernandez <joriefefernandez@yahoo.com>
2019-06-24 16:52:38 -0700

    Added calculation and test

---
+++ src/test/java/com/example/teamboolean/apprentidash/DayTest.java
@@ -0,0 +1,15 @@
+package com.example.teamboolean.apprentidash;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+public class DayTest {
+
+    @Test
+    public void calculateDailyHours() {
+
+    }
+}
