1bb1746da8eaef3c9118a0a8f57873eaf7f13359
williams osunkwo <wosunkwo@gmail.com>
2019-06-24 15:45:07 -0700

    finished the buttons for the punch in page, and able to call a function when the button is clicked

--- src/main/java/com/example/teamboolean/apprentidash/ApprentiDashController.java
+++ src/main/java/com/example/teamboolean/apprentidash/ApprentiDashController.java
@@ -8,8 +8,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
 
 @Controller
 public class ApprentiDashController {
@@ -19,6 +23,9 @@ public class ApprentiDashController {
 
     @Autowired
     PasswordEncoder passwordEncoder;
+
+    @Autowired
+    DayRepository dayRepository;
 
     @GetMapping("/")
     public String getHome(){
@@ -43,4 +50,51 @@ public class ApprentiDashController {
         SecurityContextHolder.getContext().setAuthentication(authentication);
         return "redirect:/";
     }
+
+    //****** The controller methods to handle our Punch In page ******/
+    @GetMapping("/recordHour")
+    public String recordHour(){
+        return "recordHour";
+    }
+
+    @PostMapping(value="/recordHour", params="clockIn=clockInValue")
+    public ModelAndView clockInSave() {
+        ModelAndView modelAndView = new ModelAndView();
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(dtf.format(now));
+        
+        return modelAndView;
+    }
+
+    @PostMapping(value="/recordHour", params="lunchIn=lunchInValue")
+    public ModelAndView lunchInSave() {
+        ModelAndView modelAndView = new ModelAndView();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(dtf.format(now));
+        return modelAndView;
+    }
+
+    @PostMapping(value="/recordHour", params="lunchOut=lunchOutValue")
+    public ModelAndView lunchOutSave() {
+        ModelAndView modelAndView = new ModelAndView();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(dtf.format(now));
+        return modelAndView;
+    }
+
+    @PostMapping(value="/recordHour", params="clockOut=clockOutValue")
+    public ModelAndView clockOutSave() {
+        ModelAndView modelAndView = new ModelAndView();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(dtf.format(now));
+        return modelAndView;
+    }
+
+
+
 }
