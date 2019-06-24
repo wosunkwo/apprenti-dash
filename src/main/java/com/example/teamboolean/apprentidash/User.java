fed2fbaab9f77a9606638a5769fd05a4951cfbe8
Jorie Fernandez <joriefefernandez@yahoo.com>
2019-06-24 10:48:07 -0700

    Created User and repository classes

--- src/main/java/com/example/teamboolean/apprentidash/User.java
+++ src/main/java/com/example/teamboolean/apprentidash/User.java
@@ -1,9 +1,13 @@
 package com.example.teamboolean.apprentidash;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
+import java.util.Collection;
 
 @Entity
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -62,5 +66,30 @@ public class User {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
--- src/main/java/com/example/teamboolean/apprentidash/UserRepository.java
+++ src/main/java/com/example/teamboolean/apprentidash/UserRepository.java
@@ -3,4 +3,5 @@ package com.example.teamboolean.apprentidash;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+    User findByUsername(String username);
 }
