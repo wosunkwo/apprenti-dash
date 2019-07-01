3c2be5d4de65fdfced527af6745e14d50e301198
williams osunkwo <wosunkwo@gmail.com>
2019-07-01 10:19:42 -0700

    added a few comments from peer review

--- src/main/java/com/example/teamboolean/apprentidash/WebSecurityConfig.java
+++ src/main/java/com/example/teamboolean/apprentidash/WebSecurityConfig.java
@@ -37,7 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 //allow requests to all urls that match the pattern
-                .antMatchers("/", "/home", "/signup", "/login", "/*.css", "/images/*.jpg", "/recordHour").permitAll()
+                .antMatchers("/", "/home", "/signup", "/login", "/*.css", "/images/*.jpg").permitAll()
                 .antMatchers(HttpMethod.GET, "/*.css", "/images/*.jpg", "/error").permitAll()
                 //anything else you must be logged in
                 .anyRequest().authenticated()
